8348e27c275b70770143ae4101607932b15f2bfb
Poren Chiang <ren.chiang@gmail.com>
2017-10-23 18:34:37 -0400

    Tweak title and paragraph in English

--- _/localize.js
+++ _/localize.js
@@ -2,7 +2,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     var replace = (function(attr, f) {
         var l = document.querySelectorAll('['+ attr +']');
-        for (var i in l) f(l[i], l[i].getAttribute(attr));
+        for (var i = 0; i < l.length; i++) f(l[i], l[i].getAttribute(attr));
     });
 
     var languages = navigator.languages || ([ navigator.language || navigator.userLanguage ]);
@@ -11,4 +11,5 @@ document.addEventListener('DOMContentLoaded', function() {
 
     replace('data-l10n', function(e, str) { e.innerText = str; });
     replace('data-l10n-alt', function(e, str) { e.setAttribute('alt', str); });
+    document.documentElement.setAttribute('lang', 'en');
 });
