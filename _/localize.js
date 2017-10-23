cb2079ab844cf587b5d47f3c95983419f7ca6d4a
Poren Chiang <ren.chiang@gmail.com>
2017-10-23 18:58:38 -0400

    Tweak font and English wording

--- _/localize.js
+++ _/localize.js
@@ -1,11 +1,11 @@
 'use strict';
-document.addEventListener('DOMContentLoaded', function() {
+var localize = (function(languages) {
     var replace = (function(attr, f) {
         var l = document.querySelectorAll('['+ attr +']');
         for (var i = 0; i < l.length; i++) f(l[i], l[i].getAttribute(attr));
     });
 
-    var languages = navigator.languages || ([ navigator.language || navigator.userLanguage ]);
+    languages = languages || navigator.languages || ([ navigator.language || navigator.userLanguage ]);
     for (var i in languages)
         if (/^zh/i.test(languages[i])) return;
 
@@ -13,3 +13,4 @@ document.addEventListener('DOMContentLoaded', function() {
     replace('data-l10n-alt', function(e, str) { e.setAttribute('alt', str); });
     document.documentElement.setAttribute('lang', 'en');
 });
+document.addEventListener('DOMContentLoaded', localize);
